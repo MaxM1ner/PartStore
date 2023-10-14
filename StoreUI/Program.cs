00b84d26861200c9ffd37e7b625b750a457956d0
Stecser <kravchykartem29@gmail.com>
2023-10-14 23:39:29 +0300

    Roles Conf

--- StoreUI/Program.cs
+++ StoreUI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using StoreUI.Data;
+using StoreUI.Models;
+using System.Net;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +15,33 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+//builder.Services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<Customer, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders().AddDefaultUI().AddDefaultTokenProviders();
+
+builder.Services.AddAuthorization(opts =>
+{
+    opts.AddPolicy("Admin", policy =>
+    {
+        policy.RequireRole("Admin");
+    });
+    opts.AddPolicy("User", policy =>
+    {
+        policy.RequireRole("User");
+    });
+    
+});
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Identity/Account/Login";
+        options.AccessDeniedPath = "/";
+    });
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -36,8 +66,17 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+  name: "Admin",
+  pattern: "{area:exists}/{controller=Roles}/{action=Index}/{id?}"
+);
+
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+
 app.MapRazorPages();
 
 app.Run();
