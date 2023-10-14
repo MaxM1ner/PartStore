5218f5f12163ecfca704dfca3835e16602a5131b
MaxM1ner <46611395+MaxM1ner@users.noreply.github.com>
2023-10-14 18:21:28 +0300

    Initial commit

--- StoreUI/Data/ApplicationDbContext.cs
+++ StoreUI/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace StoreUI.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
