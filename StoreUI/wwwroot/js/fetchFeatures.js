a2f2a5b404ff7b6cadd0c6964144f458edb99a42
MaxM1ner <46611395+MaxM1ner@users.noreply.github.com>
2023-10-31 20:11:27 +0200

    Fixed Js

--- StoreUI/wwwroot/js/fetchFeatures.js
+++ StoreUI/wwwroot/js/fetchFeatures.js
@@ -1,17 +1,46 @@
 const typeMenu = document.querySelector("#typeId");
 const list = document.querySelector("#fetchedFeatures");
-const selected_List = document.querySelector("#selectedFeatures");
-typeMenu.addEventListener("change", async function () {  
-    list.innerHTML = "";
-    selected_List.innerHTML = "";
-    let fetchString = `GetFeatures/${typeMenu.options[typeMenu.selectedIndex].value}`;
-    var res = await fetch(fetchString, { credentials: "include" });
-    const jsonResponse = await res.json();
-    const elements = jsonResponse.map(o => ({ name: `${o.name}: ${o.value}`, id: o.id }));
-    for (let id in elements) {
-        const option = document.createElement('option');
-        option.value = elements[id].id;
-        option.text = elements[id].name;
-        list.append(option); 
-    }
+const selectedList = document.querySelector("#selectedFeatures");
+const addBtn = document.querySelector("#add-features");
+const remBtn = document.querySelector("#remove-features");
+
+document.addEventListener('DOMContentLoaded', function () {
+    addBtn.addEventListener("click", async function () {
+        let selected = [];
+
+        for (let i = 0; i < list.selectedOptions.length; i++) {
+            selected[i] = list.selectedOptions[i];
+        }
+
+        for (var i = 0; i < selected.length; i++) {
+            selectedList.append(selected[i]);
+        }
+    });
+
+    remBtn.addEventListener("click", async function () {
+        let selected = [];
+
+        for (let i = 0; i < selectedList.selectedOptions.length; i++) {
+            selected[i] = selectedList.selectedOptions[i];
+        }
+
+        for (var i = 0; i < selected.length; i++) {
+            list.append(selected[i]);
+        }
+    });
+    typeMenu.addEventListener("change", async function () {
+        list.innerHTML = "";
+        selectedList.innerHTML = "";
+        let fetchString = `GetFeatures/${typeMenu.options[typeMenu.selectedIndex].value}`;
+        var res = await fetch(fetchString, { credentials: "include" });
+        const jsonResponse = await res.json();
+        const elements = jsonResponse.map(o => ({ name: `${o.name}: ${o.value}`, id: o.id }));
+        for (let id in elements) {
+            const option = document.createElement('option');
+            option.value = elements[id].id;
+            option.text = elements[id].name;
+            list.append(option);
+        }
+    });
 });
+
