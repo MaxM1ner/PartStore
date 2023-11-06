677a788d3f2135547264364fc811dc1c3146d37e
Sviatoslav <svyatoslav200104@gmail.com>
2023-11-07 00:24:29 +0200

    Add new styles and new background.

---
+++ StoreUI/wwwroot/js/app.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "#animation",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x1d9376,
+  backgroundColor: 0x131320,
+  points: 14.00,
+  maxDistance: 21.00,
+  spacing: 19.00
+});
