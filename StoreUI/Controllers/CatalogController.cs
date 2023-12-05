36b6e819a9f47a68923429ca2ccfd2dd95235dd4
Stecser <kravchykartem29@gmail.com>
2023-12-05 14:40:18 +0200

    Catalog Fix

--- StoreUI/Controllers/CatalogController.cs
+++ StoreUI/Controllers/CatalogController.cs
@@ -1,29 +1,29 @@
 ﻿using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Packaging;
+using ServiceContracts;
 using ServiceContracts.DTO.Product;
-using Services;
 using StoreUI.ViewModels;
 
 namespace StoreUI.Controllers
 {
     public sealed class CatalogController : Controller
     {
-        private readonly ProductManager _productManager;
-        private readonly ProductTypeManager _productTypeManager;
-        public CatalogController(ProductManager productManager, ProductTypeManager productTypeManager)
+        private readonly IProductService _productManager;
+        private readonly IProductTypeService _productTypeManager;
+        public CatalogController(IProductService productManager, IProductTypeService productTypeManager)
         {
             _productManager = productManager;
             _productTypeManager = productTypeManager;
         }
         public async Task<IActionResult> Index(int? typeid)
         {
-            var catalogProducts = new HashSet<ProductResponse>();
+            var catalogProducts = new List<ProductResponse>();
             var types = await _productTypeManager.GetProductTypesAsync();
             var products = await _productManager.GetProductsAsync();
 
             products = products.Where(x => x.Quantity > 0 && x.IsVisible).ToList();
-            if (typeid != null) { catalogProducts.AddRange(products.Where(x => x.TypeResponse.Id == typeid).ToHashSet()); }
+            if (typeid != null) { catalogProducts.AddRange(products.Where(x => x.Id == typeid).ToList()); }
             else
             {
                 catalogProducts.AddRange(products);
