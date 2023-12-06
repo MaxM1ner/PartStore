ec96497432f259267af694f9d890b4d908b85315
Stecser <kravchykartem29@gmail.com>
2023-12-06 15:47:26 +0200

    Admin Orders Index

---
+++ StoreUI/Areas/Admin/ViewModels/OrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Enums;
+using Entities.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreUI.Areas.Admin.ViewModels
+{
+    public sealed class OrderViewModel
+    {
+        public OrderViewModel()
+        {
+            OrderProducts = new HashSet<Product>();
+        }
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public string Status { get; set; } = OrderStatus.Created.ToString();
+        public string Address { get; set; } = null!;
+        public string CustomerId { get; set; } = null!;
+        public string CustomerName { get; set; } = null!;
+        public ICollection<Product> OrderProducts { get; private set; }
+    }
+    
+}
